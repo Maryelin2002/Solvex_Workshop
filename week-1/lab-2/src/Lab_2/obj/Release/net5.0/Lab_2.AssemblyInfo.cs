beb949322d02d5800262771bb847b19b945bcde5
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-07-04 17:38:04 -0400

    commit

--- week-1/lab-2/src/Lab_2/obj/Release/net5.0/Lab_2.AssemblyInfo.cs
+++ week-1/lab-2/src/Lab_2/obj/Release/net5.0/Lab_2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab_2")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
